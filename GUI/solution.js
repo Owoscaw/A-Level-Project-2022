f1fd0febf4d040290d21cf4fa42d83b51a75f7f5
Owoscaw <oscarmartinpearce@gmail.com>
2022-09-04 18:00:50 +0100

    Added network display
    Added a popup window to show the network as it is being worked on

--- GUI/solution.js
+++ GUI/solution.js
@@ -6,6 +6,56 @@ let addedNodes = {};
 const possibleIcons = ["markerIcon1.jpg", "markerIcon2.jpg", "markerIcon3.jpg"];
 
 
+//used to keep track of arcs 
+class AdjTable {
+  allNodes;
+  allArcs;
+  table;
+
+  constructor(){
+
+    this.allNodes = [];
+    this.allArcs = [];
+    this.table = {};
+  }
+
+  //adds a node to allNodes, and gives it a field in table
+  addNode(node){
+
+    this.table[node.name] = {}
+    this.allNodes.push(node);
+
+    this.populateTable();
+  }
+
+  //defines a non-zero arc between two nodes
+  addArc(weight, node1, node2){
+
+    if(node1.name === node2.name){
+      return;
+    }
+
+    this.table[node1.name][node2.name] = weight;
+    this.table[node2.name][node1.name] = weight;
+
+    this.allArcs.push({node1: node1.name, node2: node2.name, weight: weight});
+  }
+
+  //populates the table with default arcs with weight 0
+  populateTable(){
+    for(let i = 0; i < this.allNodes.length; i++){
+
+      let iName = this.allNodes[i].name;
+      for(let j = 0; j < this.allNodes.length; j++){
+
+        let jName = this.allNodes[j].name;
+        if((typeof this.table[iName][jName] === "undefined") && (iName != jName)){
+          this.table[iName][jName] = 0;
+        }
+      }
+    }
+  }
+}
 
 
 //initialising map 
@@ -27,6 +77,8 @@ function initMap() {
     if(Object.keys(addedNodes).length < 10){
       //call addMarker to add the node to the map
       addNode(event.latLng);
+    } else {
+      document.getElementById("statusContent").innerHTML = "Too many nodes!";
     }
   });
 
@@ -75,6 +127,7 @@ function initMap() {
 
           //adding it to the list of added nodes
           addedNodes[this.name] = this;
+          updateStatus();
 
           //redraw it
           this.draw();
@@ -130,6 +183,9 @@ function initMap() {
       return this.timeout;
     }
   }
+
+
+
 
   //adds a new node to the map
   function addNode(location){
@@ -188,12 +244,12 @@ function initMap() {
       //standard node deletion
       deleteNode(newNode.name, newNode, newMarker);
     });
-
-    console.log(newNode);
-
   }
 
-  //gets lat and lng values to parse to the TSP solver, and adds the node to the page
+
+
+
+  //manipulates page to add a new node
   function activateNode(name, node, marker){
 
     let parentList = document.getElementById("listOfNodes");
@@ -319,6 +375,7 @@ function initMap() {
           node.div.style.backgroundColor = "rgba(251, 188, 5, 0.5)";
           startNodeButton.className = "StartNodeButton";
 
+          updateStatus();
           return;
         } 
       }
@@ -326,6 +383,7 @@ function initMap() {
       //setting the node that was clicked to the new startnode, altering css
       startNode = node;
       startNodeButton.className = "ActiveStartNode";
+      updateStatus();
 
       //setting the startnode at the bottom of the page
       document.getElementById("startNodeName").innerHTML = name;
@@ -351,6 +409,9 @@ function initMap() {
       startNodeButton.disabled = false;
     });
   }
+
+
+
 
   //removes a node from the map, list of active markers, list of active nodes, and from the list of nodes
   function deleteNode(name, node, marker){
@@ -382,6 +443,11 @@ function initMap() {
       document.getElementById(name + " renameButton").disabled = true;
       document.getElementById(name + " startNodeButton").disabled = true;
 
+
+      //updating list of nodes and status
+      delete addedNodes[name];
+      updateStatus();
+
       //animating the removal of the div
       childDiv.classList.add("RemoveNode");
       childDiv.addEventListener("animationend", () => {
@@ -392,16 +458,248 @@ function initMap() {
         deletedNode.addEventListener("animationend", (event) => {
           if(event.animationName === "removeListAnimation"){
             parentList.removeChild(deletedNode);
-            
-            delete addedNodes[name];
           }
         });
       });
     }
   }
 
-  console.log(currentMarkers);
-  console.log(addedNodes);
+
+
+  //calculate button functionality
+  let calcButton = document.getElementById("calculateButton");
+  calcButton.addEventListener("click", () => {
+
+    //prevent spam clicking the button
+    calcButton.disabled = true;
+
+    //formatting distance matrix service request
+    let mapAdjTable = new AdjTable;
+    let nodeArray = [];
+    let toBeArced = [];
+
+    //getting nodes into an array
+    for(var node in addedNodes){
+      if(addedNodes.hasOwnProperty(node)){
+        nodeArray.push(addedNodes[node]);
+      }
+    }
+
+    //populating an adjacency table with default arc weights
+    for(var i = 0; i < nodeArray.length; i++){
+    
+      let currentNode = nodeArray[i];
+
+      mapAdjTable.addNode(currentNode);
+      toBeArced[i] = currentNode.point;
+    }
+
+
+    getMatrix(toBeArced, nodeArray, mapAdjTable);
+
+    displayLoadingMenu();
+
+    updateLoadingMenu(mapAdjTable, "Initial Network");
+  });
+}
+
+
+//updates the status of the page
+function updateStatus(){
+
+  //detecting if a starting node has been selected
+  let startNodeSelected = false;
+  if(typeof startNode != "undefined"){
+    startNodeSelected = true;
+  }
+
+  //detecting if there are enough nodes for a viable solution
+  let enoughNodes = false;
+  if(Object.keys(addedNodes).length > 2){
+    enoughNodes = true;
+  }
+
+  //getting status box where the status will be updated
+  let statusBox = document.getElementById("statusContent");
+
+  if(!enoughNodes){
+    statusBox.innerHTML = "Add more than two nodes"
+  } else if(!startNodeSelected){
+    statusBox.innerHTML = "Select starting node";
+  } else {
+    statusBox.innerHTML = "Ready to solve";
+  }
+}
+
+
+//function that gets distances between nodes, needs to be here bc asyncronous work??
+function getMatrix(toBeArced, nodeArray, adjTable){
+
+  //optimal way of getting every node, adding it to the adjTable
+  for(var j = 0; j < toBeArced.length - 1; j++){
+
+    let currentOrigin = toBeArced[j];
+    let currentDestinations = toBeArced.slice(j + 1);
+
+    //getting the names of all the destination nodes
+    let fromNode = nodeArray[j];
+    let toNodes = nodeArray.slice(j + 1);
+
+    // var matrix = new google.maps.DistanceMatrixService();
+    // matrix.getDistanceMatrix({
+    //   origins: [currentOrigin],
+    //   destinations: currentDestinations,
+    //   travelMode: "DRIVING"
+    // }, function(response, status){
+
+    //   //allows me to tell if matrix failed outside of this function
+    //   if(status === "OK"){
+
+    //     //successful request, inputting response into adjTable class
+    //     for(var sink = 0; sink < response.rows[0].elements.length; sink++){
+
+    //       //getting the arc's connection
+    //       let toNode = toNodes[sink];
+
+    //       //adding the arc to the adjacency table
+    //       adjTable.addArc(response.rows[0].elements[sink].distance.value, fromNode, toNode);
+    //     }
+    //   }
+    // });
+
+    adjTable.addArc(1, fromNode, toNodes[0]);
+  }
+
+  console.log(adjTable);
+}
+
+
+
+//dims the page and display the graph as it is being worked on
+function displayLoadingMenu(){
+
+  let upperPage = document.getElementById("UpperPage");
+  let lowerPage = document.getElementById("LowerPage");
+  upperPage.style.pointerEvents = "none";
+  lowerPage.style.pointerEvents = "none";
+
+  let cover = document.createElement("div");
+  cover.id = "cover";
+
+  let canvasDiv = document.createElement("div");
+  canvasDiv.id = "canvasDiv";
+
+  let cancelButton = document.createElement("input");
+  cancelButton.id = "cancelButton";
+  cancelButton.type = "button";
+  cancelButton.value = "×";
+
+  //cancel functionality
+  cancelButton.addEventListener("click", () => {
+    document.getElementById("coverDiv").removeChild(cover);
+    upperPage.style.pointerEvents = "auto";
+    lowerPage.style.pointerEvents = "auto";
+    document.getElementById("calculateButton").disabled = false;
+  });
+
+  let networkCanvas = document.createElement("canvas");
+  networkCanvas.id = "networkCanvas";
+
+  //adding all elements
+  canvasDiv.appendChild(cancelButton);
+  canvasDiv.appendChild(networkCanvas);
+  cover.appendChild(canvasDiv);
+  document.getElementById("coverDiv").appendChild(cover);
+}
+
+
+
+function updateLoadingMenu(adjTable, title){
+
+  //checking if the cancel button has been pressed
+  if(!document.getElementById("calculateButton").disabled){
+    return;
+  }
+
+  let networkTitle = document.createElement("div");
+  networkTitle.id = "networkTitle";
+  networkTitle.innerHTML = title;
+  document.getElementById("canvasDiv").appendChild(networkTitle);
+
+  let networkCanvas = document.getElementById("networkCanvas");
+  networkCanvas.width = 850;
+  networkCanvas.height = 425;
+
+  //clearing the canvas
+  let networkContext = networkCanvas.getContext("2d");
+  networkContext.clearRect(0, 0, networkCanvas.width, networkCanvas.height);
+
+  //defining font and font size for nodes on canvas
+  networkContext.font = "20px Google Sans";
+  networkContext.fillStyle = "black";
+  networkContext.textAlign = "center";
+  networkContext.direction = "ltr";
+
+  //generating x and y coordinates to place each node at, and drawing them
+  let nodeXY = {};
+  for(var i = 0; i < adjTable.allNodes.length; i++){
+
+    let tooClose = true;
+
+    let currentX = 50 + Math.random()*(networkCanvas.width - 100);
+    let currentY = 25 + Math.random()*(networkCanvas.height - 50);
+
+    //avoiding placing nodes too close to each other
+    while(tooClose){
+
+      //coordinates can be within 50px of the canvas X wise, 25px Y wise
+      currentX = 50 + Math.random()*(networkCanvas.width - 100);
+      currentY = 25 + Math.random()*(networkCanvas.height - 50);
+      tooClose = false;
+
+      //checking if these coordinates are too close to others
+      for(var j in nodeXY){
+        if(nodeXY.hasOwnProperty(j)){
+          let nearX = nodeXY[j].x;
+          let nearY = nodeXY[j].y;
+
+          let xDifference = Math.abs(nearX - currentX);
+          let yDifference = Math.abs(nearY - currentY);
+
+          //using pythagors theorom, who said we'd never use it?
+          let proximity = Math.sqrt(xDifference**2 + yDifference**2);
+
+          //constant fine tuned
+          if(proximity < 75){
+            tooClose = true;
+          }
+        }
+      }
+    }
+
+    networkContext.fillText(adjTable.allNodes[i].name, currentX, currentY);
+    nodeXY[adjTable.allNodes[i].name] = {x: currentX, y: currentY};
+  }
+
+  //adding the arcs to the canvas
+  for(var k = 0; k < adjTable.allArcs.length; k++){
+
+    console.log("test");
+
+    //getting the points to connect
+    let node1X = nodeXY[adjTable.allArcs[k].node1].x;
+    let node1Y = nodeXY[adjTable.allArcs[k].node1].y;
+
+    let node2X = nodeXY[adjTable.allArcs[k].node2].x;
+    let node2Y = nodeXY[adjTable.allArcs[k].node2].y;
+
+    //drawing the line
+    networkContext.beginPath();
+    networkContext.moveTo(node1X, node1Y);
+    networkContext.lineTo(node2X, node2Y);
+    networkContext.stroke();
+  }
+
 }
 
 window.initMap = initMap;
